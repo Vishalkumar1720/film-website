3781c000cf3cc5c62aafddd169b2aff95f112856
Vishal kumar <92419255+Vishalkumar1720@users.noreply.github.com>
2026-09-22 22:39:24 +0530

    for vercel config changed

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
-  base: '/film-website/',
+  base: '/',
 
   build: {
     rollupOptions: {
